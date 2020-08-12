515c5b0fab00ce5d69cc16dcb0cfcef111da8d1e
umarbhatt1 <umarbhatti445@gmail.com>
2020-08-13 00:56:23 +0500

    remove jdk11

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,6 @@
 set -x
 for f in $(find test/unit -type f | head -2); do
   t="$(basename $f .java)"
-  echo ant -Duse.jdk11=true test -Dtest.name="$t"
-  ant -Duse.jdk11=true test -Dtest.name="$t"
+  echo ant test -Dtest.name="$t"
+  ant test -Dtest.name="$t"
 done
