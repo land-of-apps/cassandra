f36500ddf40910da8f60fa7570aa4c0eff1ee549
umarbhatt1 <umarbhatti445@gmail.com>
2020-08-13 01:05:02 +0500

    running all tests from script

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -x
-for f in $(find test/unit -type f | head -2); do
+for f in $(find test/unit -type f); do
   t="$(basename $f .java)"
-  echo ant test -Dtest.name="$t"
   ant test -Dtest.name="$t"
 done
