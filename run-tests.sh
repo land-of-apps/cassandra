6d44a7bd9043ae59f7d9e782845a5e77d3b04d4a
umarbhatt1 <umarbhatti445@gmail.com>
2020-08-13 01:49:29 +0500

    excluding keycachetest

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-for f in $(find test/unit/org/apache/cassandra/db -type f); do
+for f in $(find test/unit/org/apache/cassandra/db -type f grep -v -e KeyCacheTest); do
   t="$(basename $f .java)"
   ant test -Dtest.name="$t"
 done
